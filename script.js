99d461cadc44f931f9d9a8d0edc81863a08a3144
GavvaSrivalli <157472049+GavvaSrivalli@users.noreply.github.com>
2025-12-27 17:21:35 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,95 @@
+let container = document.getElementById("bg-container");
+let createtask = document.getElementById("createtask");
+let ulist = document.getElementById("ulist");
+
+let uniqueid = 1;
+
+function loadTasks() {
+    let stored = localStorage.getItem("todoTasks");
+    if (!stored) return;
+
+    let tasks = JSON.parse(stored);
+
+    for (let i = 0; i < tasks.length; i++) {
+        createtask.value = tasks[i].text;
+        addBtn(tasks[i].checked);
+    }
+}
+loadTasks();
+
+
+function SaveBtn() {
+    let tasks = [];
+
+    let labels = document.getElementsByClassName("task-list");
+    let checkboxes = document.getElementsByClassName("checkbox");
+
+    for (let i = 0; i < labels.length; i++) {
+        tasks.push({
+            text: labels[i].textContent,
+            checked: checkboxes[i].checked
+        });
+
+    }
+    localStorage.setItem("todoTasks", JSON.stringify(tasks));
+}
+
+function addBtn(isChecked = false) {
+    let list = document.createElement("li");
+    list.classList.add("d-flex", "flex-row");
+
+    if (createtask.value === "") {
+        alert("Enter valid text");
+        return;
+    }
+
+    let todoId = "todo" + uniqueid;
+    let checkboxId = "checkbox" + uniqueid;
+    let LabelId = "label" + uniqueid;
+
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = checkboxId;
+    checkbox.classList.add("checkbox");
+    checkbox.checked = isChecked;
+
+    let labelContainer = document.createElement("div");
+    labelContainer.classList.add("labelContainer", "d-flex", "flex-row");
+
+    let label = document.createElement("label");
+    label.textContent = createtask.value;
+    label.htmlFor = checkboxId;
+    label.classList.add("task-list");
+    labelContainer.appendChild(label);
+    label.id = LabelId;
+
+    if (isChecked) {
+        label.classList.add("checked");
+    }
+
+    let deleteContainer = document.createElement("div");
+    deleteContainer.classList.add("delete-icon-container");
+    labelContainer.appendChild(deleteContainer);
+
+    let deleteicon = document.createElement("i");
+    deleteicon.classList.add("fa-solid", "fa-trash-can", "delete-icon");
+    deleteContainer.appendChild(deleteicon);
+
+    deleteicon.addEventListener("click", function() {
+        list.remove();
+    });
+
+    checkbox.addEventListener("change", function() {
+        label.classList.toggle("checked");
+    });
+
+
+    list.appendChild(checkbox);
+    list.appendChild(labelContainer);
+
+    ulist.appendChild(list);
+    uniqueid = uniqueid + 1;
+
+    createtask.value = "";
+}
